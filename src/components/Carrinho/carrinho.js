872665b73067cb714e81315e5b160223ace7e365
Wener Simonassi Borges Pacheco <wenersimonassi@gmail.com>
2022-07-19 22:16:28 -0300

    codigo-base-carrinho

--- src/components/Carrinho/carrinho.js
+++ src/components/Carrinho/carrinho.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {CarrinhoContainer} from "./style"
 
-export function Carrinho(){
+/*export function Carrinho(){
       return <CarrinhoContainer>
            <></>
 
@@ -11,4 +11,39 @@ export function Carrinho(){
 
 
 
+}*/
+
+const [produtosCarrinho, setProdutosCarrinho] = useState([]);
+
+
+function compraProduto(nome){
+    const copiaCompraProduto = [...compraProduto];
+const novoProduto= copiaCompraProduto.find((produto)=>produto.nome=nome);
+if(!novoProduto){
+    copiaCompraProduto.push({nome:nome, quantidade:1});
+}else{
+    novoProduto.quantidade = produto.quantidade +1
 }
+setProdutosCarrinho(copiaCompraProduto);
+}
+
+function removeProduto(nome){
+    const copiaCompraProduto = [...compraProduto];
+    const novoProduto= copiaCompraProduto.find((produto)=>produto.nome=nome);
+
+if(produto.quantidade >1){
+    novoProduto.quantidade = produto.quantidade - 1;
+    setProdutosCarrinho(copiaCompraProduto);
+}else{  
+    const arrayFiltered = copiaCompraProduto.filter(produto => produto.nome != nome )
+}
+
+setProdutosCarrinho(arrayFiltered)
+}
+
+return(
+<CarrinhoContainer
+value={{produtoCard, compraProduto, removeProduto}}>
+
+</CarrinhoContainer>
+);
