c84957c80e930b57558d2f082c9e2623272d0301
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-07-19 22:39:20 -0300

    alterando filtros

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React from 'react';
 import {HomeContainer} from "./style"
 
 
-export function Home(){
-      return <HomeContainer>
-            <></>
+export const Home=(props)=>{
+      const componentsCards=props.meusCards.map((item, index)=>{
+      return <li key={index}>
+            {item.name}
+            {item.price}
+            {item.img}
+            </li>
+      
+})
+
+      return ( 
+      <HomeContainer>
+            {componentsCards}
       </HomeContainer>
-
-
-
+      )
 }
+
+export default Home;
